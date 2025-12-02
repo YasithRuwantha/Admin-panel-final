c32005db71eb8ed909c630769d65430f747b4d85
YasithRuwantha <yasithruwantha119@gmail.com>
2025-12-02 18:10:28 +0530

    feat : added edit button for projects;

--- application/models/Project_model.php
+++ application/models/Project_model.php
@@ -2,6 +2,14 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Project_model extends CI_Model {
+            public function get_project_by_id($id) {
+                return $this->db->get_where('project', ['id' => $id])->row_array();
+            }
+
+            public function update_project($id, $data) {
+                $this->db->where('id', $id);
+                return $this->db->update('project', $data);
+            }
         public function project_code_exists($project_code) {
             return $this->db->where('project_code', $project_code)->count_all_results('project') > 0;
         }
--- application/views/list_projects.php
+++ application/views/list_projects.php
@@ -21,7 +21,7 @@
                     <th>Address</th>
                     <th>Project Value</th>
                     <th>Start Date</th>
-                    <th>Status</th>
+                    <th>Status</th>                   
                 </tr>
             </thead>
             <tbody>
@@ -36,6 +36,9 @@
                             <td style="word-break:break-word;max-width:180px;white-space:pre-line;"><?php echo htmlspecialchars($project['paysheet_value']); ?></td>
                             <td style="word-break:break-word;max-width:180px;white-space:pre-line;"><?php echo htmlspecialchars($project['start_date']); ?></td>
                             <td style="word-break:break-word;max-width:180px;white-space:pre-line;"><?php echo htmlspecialchars($project['status']); ?></td>
+                            <td style="word-break:break-word;max-width:120px;white-space:pre-line;">
+                                <a href="<?php echo site_url('project/edit/' . $project['id']); ?>" class="btn btn-sm btn-warning"><i class="bi bi-pencil-square"></i> Edit</a>
+                            </td>
                         </tr>
                     <?php endforeach; ?>
                 <?php else: ?>
