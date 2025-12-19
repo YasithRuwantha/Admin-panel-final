b1645519403f693b275bb6b1b1066ac54a85a124
YasithRuwantha <yasithruwantha119@gmail.com>
2025-12-19 14:44:55 +0530

    feat : added add user button ;

--- application/views/add_expense.php
+++ application/views/add_expense.php
@@ -86,7 +86,10 @@
                             </select>
                         </div>
                         <div class="col">
-                            <label>Paid By</label>
+                            <div class="d-flex align-items-center mb-1">
+                                <label class="me-2">Paid By</label>
+                                <button type="button" class="btn btn-link p-0" id="addPaidByBtn" title="Add Paid By"><i class="bi bi-plus-circle" style="font-size:1.2rem;"></i></button>
+                            </div>
                             <select name="paid_by" class="form-control" required>
                                 <option value="">Select</option>
                                 <?php if (!empty($paid_by_options)) : ?>
